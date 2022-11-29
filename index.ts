6d7a9207dee6f0388a258a907c50b62ab2e8c59a
Karkachevich <karkachevichtv@yandex.ru>
2022-11-29 15:02:59 +0500

    feat: added types createServer

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 import http from "http";
+import { IncomingMessage, ServerResponse } from "http";
 
-const server = http.createServer(() => {
-    console.log('пришёл запрос!')
-});
+const server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    res.writeHead(200, {
+      "Content-Type": "text/html; charset=utf8",
+    });
+    res.end("<h1>Привет, мир!</h1>", "utf-8");
+  }
+);
 
 server.listen(3000);
-
