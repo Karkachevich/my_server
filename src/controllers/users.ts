e7c24ca86144daa48c58fe0d53e5b37bdb84f3db
Karkachevich <karkachevichtv@yandex.ru>
2022-12-23 15:07:16 +0500

    feat: added getUsers

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,1 +1,6 @@
+import { Response, Request, NextFunction } from 'express';
+import User from '../models/user';
 
+export const getUsers = (req: Request, res: Response, next: NextFunction) => User.find({})
+  .then((users) => res.send({ data: users }))
+  .catch(next);
