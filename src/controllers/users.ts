e97cf9b77ec1dff27b4d76c728152f6521d253a4
Karkachevich <karkachevichtv@yandex.ru>
2022-12-24 17:10:58 +0500

    feat: added getUserById

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,23 @@
 import { Response, Request, NextFunction } from 'express';
+import BadRequestError from '../errors/bad-request-error';
+import NotFoundError from '../errors/not-found-error';
 import User from '../models/user';
 
 export const getUsers = (req: Request, res: Response, next: NextFunction) => User.find({})
   .then((users) => res.send({ data: users }))
   .catch(next);
+
+  export const getUserById = (req: Request, res: Response, next: NextFunction) => {
+    User.findById(req.params.userId)
+      .orFail(new NotFoundError('Нет пользователя'))
+      .then((user) => {
+        res.send({ data: user });
+      })
+      .catch((err) => {
+        if (err.name === 'CastError') {
+          next(new BadRequestError('Некорректные данные'));
+        } else {
+          next(err);
+        }
+      });
+  };
