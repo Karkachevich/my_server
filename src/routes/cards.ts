b6e5fb28277a92d02ff95050a06c0758f7d5e8fb
Karkachevich <karkachevichtv@yandex.ru>
2023-01-02 16:20:19 +0500

    feat: added routes cards

--- src/routes/cards.ts
+++ src/routes/cards.ts
@@ -1,1 +1,19 @@
 import { Router } from 'express';
+import {
+  getCards,
+  createCard,
+  deleteCard,
+  likeCard,
+  dislikeCard,
+} from '../controllers/cards';
+import { validateCreateCard, validateCardId } from '../validation/card';
+
+const router = Router();
+
+router.get('/', getCards);
+router.post('/', validateCreateCard, createCard);
+router.delete('/:cardId', validateCardId, deleteCard);
+router.put('/:cardId/likes', validateCardId, likeCard);
+router.delete('/:cardId/likes', validateCardId, dislikeCard);
+
+export default router;
